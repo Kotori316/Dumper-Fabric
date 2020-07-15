01debbd35b91086e5acbaf99af755a1b18bea583
Kotori316 <Kotori316@gmail.com>
2020-07-15 22:23:08 +0900

    Markdown table style

--- src/main/scala/com/kotori316/dumper/dumps/Formatter.scala
+++ src/main/scala/com/kotori316/dumper/dumps/Formatter.scala
@@ -9,12 +9,24 @@ class Formatter[A](rows: Seq[String], converters: Seq[A => Any]) {
     val lengthMaxes: Seq[Int] = (rows +: converted).map(_.map(_.length)).foldLeft(Seq.fill(rows.size)(0)) {
       case (s1, s2) => (s1 zip s2).map { case (i, i1) => i max i1 }
     }
-    val formatString = (lengthMaxes zip rows).map { case (i, str) => s"%${getMinus(str)}${i}s" }.mkString(" : ")
-    (removeMinus(rows) +: converted).map(ss => formatString.format(ss: _*))
+    val secondRow = makeSecondRow(lengthMaxes)
+    val formatString = (lengthMaxes zip rows).map { case (i, str) => s"%${getMinus(str)}${i}s" }.mkString("|", " | ", "|")
+    (removeMinus(rows) +: secondRow +: converted).map(ss => formatString.format(ss: _*))
       .map(s => ("0" + s).trim.substring(1))
   }
 
   def getMinus(s: String): String = if (s.startsWith("-")) "-" else ""
 
   def removeMinus(ss: Seq[String]): Seq[String] = ss.map(s => if (s.startsWith("-")) s.tail else s)
+
+  def makeSecondRow(maxes: Seq[Int]): Seq[String] = {
+    for {
+      (row, max) <- rows zip maxes
+    } yield {
+      val isLeft = row.startsWith("-")
+      val prefix = if (isLeft) ":" else ""
+      val postfix = if (isLeft) "" else ":"
+      prefix + "-" * (max - 1) + postfix
+    }
+  }
 }
